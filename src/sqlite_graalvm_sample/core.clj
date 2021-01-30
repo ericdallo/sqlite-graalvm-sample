e4805cdebe2edea20de11652b7a5a501c7c5649b
Eric Dallo <ericdallo06@hotmail.com>
2021-01-30 18:51:01 -0300

    Initial commit

---
+++ src/sqlite_graalvm_sample/core.clj
@@ -0,0 +1,6 @@
+(ns sqlite-graalvm-sample.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/sqlite_graalvm_sample/core_test.clj
@@ -0,0 +1,7 @@
+(ns sqlite-graalvm-sample.core-test
+  (:require [clojure.test :refer :all]
+            [sqlite-graalvm-sample.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
