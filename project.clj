7931e2d9736924bd256b4258e79e99e55587e501
Eric Dallo <ericdallo06@hotmail.com>
2021-02-05 14:45:25 -0300

    Fix CI

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqlite-graalvm-sample "v0.2.3"
+(defproject sqlite-graalvm-sample "v0.2.5"
   :description "A sample to test graalvm native image with sqlite/jdbc"
   :url "http://github.com/ericdallo/sqlite-graalvm-sample"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -6,10 +6,11 @@
   :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.xerial/sqlite-jdbc "3.34.0"]
                  [seancorfield/next.jdbc "1.1.613"]]
-  :profiles  {:uberjar      {:aot :all
-                             :jvm-opts ["-Dclojure.compiler.direct-linking=true"
-                                        "-Dclojure.spec.skip-macros=true"]}
-              :native-image {:dependencies [[ericdallo/sqlite-jni-graal-fix "0.0.2-graalvm-21.0.0"]
+  :jvm-opts ["-Dclojure.compiler.direct-linking=true"
+             "-Dclojure.spec.skip-macros=true"]
+  :profiles  {:uberjar      {:aot :all}
+              :native-image {:dependencies [[ericdallo/sqlite-jni-graal-fix "0.0.3-graalvm-21.0.0"
+                                             :exclusions [org.graalvm.nativeimage/svm]]
                                             [org.graalvm.nativeimage/svm "21.0.0"]]}}
   :main sqlite-graalvm-sample.core
   :repl-options {:init-ns sqlite-graalvm-sample.core})
