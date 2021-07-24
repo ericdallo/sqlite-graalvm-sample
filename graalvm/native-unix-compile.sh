6262d98cdf178aa77946771fe5d878ae0e00f566
Eric Dallo <ericdallo06@hotmail.com>
2021-07-23 22:03:05 -0300

    Update project + graalvm + config

--- graalvm/native-unix-compile.sh
+++ graalvm/native-unix-compile.sh
@@ -2,31 +2,25 @@
 
 set -e
 
+if [ -z "$GRAALVM_HOME" ]; then
+    echo "Please set GRAALVM_HOME"
+    exit 1
+fi
+
+
 if [[ ! -f "$SQLITE_GRAALVM_SAMPLE_JAR" ]]
 then
     lein with-profiles +native-image "do" clean, uberjar
-    SQLITE_GRAALVM_SAMPLE_JAR=$(ls target/sqlite-graalvm-sample-*-standalone.jar)
 fi
 
+SQLITE_GRAALVM_SAMPLE_JAR=$(ls target/sqlite-graalvm-sample-*-standalone.jar)
 SQLITE_GRAALVM_SAMPLE_XMX=${SQLITE_GRAALVM_SAMPLE_XMX:-"-J-Xmx4g"}
 
 args=("-jar" "$SQLITE_GRAALVM_SAMPLE_JAR"
-      "-H:Name=sqlite-graalvm-sample"
-      "-J-Dclojure.compiler.direct-linking=true"
-      "-J-Dclojure.spec.skip-macros=true"
       "-H:+ReportExceptionStackTraces"
-      "--enable-url-protocols=jar"
-      "-H:+InlineBeforeAnalysis"
-      "-H:Log=registerResource:"
       "--verbose"
-      "-H:IncludeResources=\"db/.*|static/.*|templates/.*|.*.yml|.*.xml|.*/org/sqlite/.*|org/sqlite/.*|.*.properties\""
-      "-H:ConfigurationFileDirectories=graalvm"
-      "--initialize-at-build-time"
-      "--report-unsupported-elements-at-runtime"
-      "--no-server"
       "--no-fallback"
       "--native-image-info"
-      "--allow-incomplete-classpath"
       "$SQLITE_GRAALVM_SAMPLE_XMX")
 
 SQLITE_GRAALVM_SAMPLE_STATIC=${SQLITE_GRAALVM_SAMPLE_STATIC:-}
@@ -35,4 +29,4 @@ if [ "$SQLITE_GRAALVM_SAMPLE_STATIC" = "true" ]; then
     args+=("--static")
 fi
 
-native-image "${args[@]}"
+"$GRAALVM_HOME/bin/native-image" "${args[@]}"
--- project.clj
+++ project.clj
@@ -1,16 +1,15 @@
-(defproject sqlite-graalvm-sample "v0.2.10"
+(defproject sqlite-graalvm-sample "v0.2.11"
   :description "A sample to test graalvm native image with sqlite/jdbc"
   :url "http://github.com/ericdallo/sqlite-graalvm-sample"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.2"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.xerial/sqlite-jdbc "3.34.0"]
                  [seancorfield/next.jdbc "1.1.613"]]
   :jvm-opts ["-Dclojure.compiler.direct-linking=true"
              "-Dclojure.spec.skip-macros=true"]
   :profiles  {:uberjar      {:aot :all}
-              :native-image {:dependencies [[ericdallo/sqlite-jni-graal-fix "0.0.3-graalvm-21.0.0"
-                                             :exclusions [org.graalvm.nativeimage/svm]]
-                                            [org.graalvm.nativeimage/svm "21.0.0"]]}}
+              :native-image {:dependencies [[ericdallo/sqlite-jni-graal-fix "0.0.3-graalvm-21.1.0"
+                                             :exclusions [org.graalvm.nativeimage/svm]]]}}
   :main sqlite-graalvm-sample.core
   :repl-options {:init-ns sqlite-graalvm-sample.core})
