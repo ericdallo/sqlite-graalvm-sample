52e90955404f4a0176ab4db494db2c6eeec96d4a
Eric Dallo <ericdallo06@hotmail.com>
2021-07-23 22:46:15 -0300

    Remove sqlite-graalvm-fix dependency using reflect and jni config instead

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sqlite-graalvm-sample "v0.2.11"
+(defproject sqlite-graalvm-sample "v0.3.0"
   :description "A sample to test graalvm native image with sqlite/jdbc"
   :url "http://github.com/ericdallo/sqlite-graalvm-sample"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -8,8 +8,6 @@
                  [seancorfield/next.jdbc "1.1.613"]]
   :jvm-opts ["-Dclojure.compiler.direct-linking=true"
              "-Dclojure.spec.skip-macros=true"]
-  :profiles  {:uberjar      {:aot :all}
-              :native-image {:dependencies [[ericdallo/sqlite-jni-graal-fix "0.0.3-graalvm-21.1.0"
-                                             :exclusions [org.graalvm.nativeimage/svm]]]}}
+  :profiles  {:uberjar {:aot :all}}
   :main sqlite-graalvm-sample.core
   :repl-options {:init-ns sqlite-graalvm-sample.core})
